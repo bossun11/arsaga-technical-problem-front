0b6ee60d0219e32f48b72cec3a1b0025f01438a6
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-01-31 15:11:44 +0900

    Change: daisyUIの使用をやめ、shadcn/uiを使用

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Button from "./Button";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <div className="flex flex-col drawer-content">
       <nav className="bg-deepRed py-3 px-5 flex items-center justify-end">
-        <Link href="/signup" className="mx-3">
-          <Button buttonText="新規登録" />
-        </Link>
-        <Link href="/login">
-          <Button buttonText="ログイン" />
-        </Link>
+        <Button className="mx-3">
+          <Link href="/signup">新規登録</Link>
+        </Button>
+        <Button>
+          <Link href="/login">ログイン</Link>
+        </Button>
       </nav>
     </div>
   );
