b620b8e48611cd1ec5e561107afa00466ac163f9
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-01-31 17:00:52 +0900

    Delete: コメントアウトしていたコードを削除

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SignUpParams } from "../app/types";
@@ -49,57 +48,6 @@ const SignUpForm = () => {
   };
 
   return (
-    // <form onSubmit={handleSubmit(onSubmit)}>
-    //   <div className="mb-2">
-    //     <label className="mb-2 text-md" htmlFor="name">
-    //       ユーザー名
-    //     </label>
-    //     <input
-    //       className="w-full px-3 py-1 border rounded-md"
-    //       id="name"
-    //       type="text"
-    //       {...register("name")}
-    //     />
-    //     {errors.name && (
-    //       <span className="text-error">{errors.name.message}</span>
-    //     )}
-    //   </div>
-
-    //   <div className="mb-2">
-    //     <label className="mb-2 text-md" htmlFor="email">
-    //       メールアドレス
-    //     </label>
-    //     <input
-    //       className="w-full px-3 py-1 border rounded-md"
-    //       id="email"
-    //       type="email"
-    //       {...register("email")}
-    //     />
-    //     {errors.email && (
-    //       <span className="text-error">{errors.email.message}</span>
-    //     )}
-    //   </div>
-
-    //   <div className="mb-2">
-    //     <label className="mb-2 text-md" htmlFor="password">
-    //       パスワード
-    //     </label>
-    //     <input
-    //       className="w-full px-3 py-1 border rounded-md"
-    //       id="password"
-    //       type="password"
-    //       {...register("password")}
-    //     />
-    //     {errors.password && (
-    //       <span className="text-error">{errors.password.message}</span>
-    //     )}
-    //   </div>
-
-    //   <div className="flex justify-center">
-    //     <Button>登録する</Button>
-    //   </div>
-    // </form>
-
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
