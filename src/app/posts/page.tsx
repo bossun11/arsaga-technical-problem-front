2a0d77df3ed3b4539f83ab70ef3f19f2c854da33
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-05 21:02:48 +0900

    Add: 取得した投稿を表示するカードを作成

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,31 @@
-const page = () => {
-  return <div>投稿一覧</div>;
-};
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 
+const page = () => {
+  return (
+    <div className=" h-screen p-4">
+      <h2 className="text-3xl m-4 text-center">投稿一覧</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <Card className="shadow-xl rounded-xl mb-5">
+          <CardHeader className="flex flex-col items-center justify-center">
+            <Image
+              src="/no_image.webp"
+              width={300}
+              height={200}
+              alt="投稿画像"
+              className="rounded-lg mb-3"
+            />
+            <CardTitle className="text-center">タイトル</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="p-5">
+              <p>ユーザー名</p>
+              <p>投稿日 2021/10/10</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
 export default page;
