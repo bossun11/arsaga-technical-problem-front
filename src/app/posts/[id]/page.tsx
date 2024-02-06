39c323736957b61d249facba6fafcd64d503ce42
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-06 17:00:14 +0900

    Add: 取得した投稿詳細情報をカードに表示

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,30 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import { Post } from "@/app/types";
+import { useParams } from "next/navigation";
 
-const page = () => {
+const Page = () => {
+  const [post, setPost] = useState<Post | null>(null);
+  const { id } = useParams();
+
+  const getPost = useCallback(async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/posts/${id}`
+    );
+    const data = await res.json();
+    setPost(data);
+  }, [id]);
+
+  useEffect(() => {
+    getPost();
+  }, [getPost]);
+
+  if (!post) return null;
+
   return (
     <div className=" h-screen p-4 flex flex-col items-center">
       <Card className="shadow-xl rounded-xl mb-5 w-1/2 hover:cursor-pointer">
@@ -15,7 +37,7 @@ const page = () => {
             className="rounded-lg mb-3"
             priority
           />
-          <CardTitle>タイトル</CardTitle>
+          <CardTitle>{post.title}</CardTitle>
           <div className="flex items-start w-full gap-1">
             <Badge className="bg-deepRed">タグ1</Badge>
             <Badge className="bg-deepRed">タグ2</Badge>
@@ -23,11 +45,11 @@ const page = () => {
         </CardHeader>
         <CardContent>
           <div className="pl-5">
-            <p>投稿内容</p>
+            <p>{post.content}</p>
           </div>
           <div className="p-5">
-            <p>ユーザー名</p>
-            <p>投稿日</p>
+            <p>{post.user.name}</p>
+            <p>投稿日 {new Date(post.created_at).toLocaleDateString()}</p>
           </div>
         </CardContent>
       </Card>
@@ -35,4 +57,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
