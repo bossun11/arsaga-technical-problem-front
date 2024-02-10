383f7a7f295e6cba1c48326c043a528eca003b4a
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-10 15:14:17 +0900

    Fix: 検索結果がなかった場合は「投稿がありません」と表示するように修正

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -25,14 +25,18 @@ const Page = () => {
   return (
     <div className=" h-screen p-4">
       <h2 className="text-3xl m-4 text-center">投稿一覧</h2>
-
       <SearchPostField setPosts={setPosts} />
-
-      <div className="grid grid-cols-3 gap-4">
-        {posts.map((post) => (
-          <PostCard key={post.id} {...post} />
-        ))}
-      </div>
+      {posts.length === 0 ? (
+        <p className="flex items-center justify-center text-3xl min-h-32">
+          投稿がありません
+        </p>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {posts.map((post) => (
+            <PostCard key={post.id} {...post} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
